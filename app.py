b36fef93c7319a45c99582f10cb5cb4a4a649d83
Maksim Manylov <maximalist888@icloud.com>
2021-05-13 20:38:22 +0300

    using redis lists for ticket comments

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy.orm import joinedload
 from flask import request
 from config import TICKET_STATUS_OPEN, TICKET_STATUS_CLOSED, TICKET_STATUS_ANSWERED, TICKET_STATUS_WAITING_FOR_ANSWER, \
-    TICKET_STATUSES
+    TICKET_STATUSES, REDIS_TICKET_COMMENTS_KEY_TEMPLATE
 import json
+import redis
+import uuid
 
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ app.config.from_object(env_config)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+r = redis.Redis(db=1)
 
 
 class Ticket(db.Model):
@@ -44,28 +46,6 @@ class Ticket(db.Model):
         }
 
 
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    email = db.Column(db.String(256), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
-    ticket = db.relationship('Ticket', backref=db.backref('comments', lazy=True))
-
-    def __repr__(self):
-        return '<Comment id={}>'.format(self.id)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'ticket_id': self.ticket_id,
-            'text': self.text,
-            'email': self.email,
-            'created_at': str(self.created_at) if self.created_at else None,
-        }
-
-
 @app.route('/tickets', methods=['GET'])
 def ticket_all():
     tickets = Ticket.query.all()
@@ -74,7 +54,7 @@ def ticket_all():
 
 @app.route('/ticket/<int:ticket_id>', methods=['GET'])
 def ticket_get(ticket_id):
-    ticket = Ticket.query.options(joinedload(Ticket.comments)).filter_by(id=ticket_id).one()
+    ticket = Ticket.query.filter_by(id=ticket_id).one()
     return ticket.to_dict()
 
 
@@ -127,28 +107,33 @@ def ticket_delete(ticket_id):
 
 @app.route('/ticket/<int:ticket_id>/comments', methods=['GET'])
 def ticket_comments_get(ticket_id):
-    ticket = Ticket.query.options(joinedload(Ticket.comments)).filter_by(id=ticket_id).one()
-    return json.dumps([c.to_dict() for c in ticket.comments])
+    comments = r.lrange(REDIS_TICKET_COMMENTS_KEY_TEMPLATE.format(ticket_id=ticket_id), 0, -1)
+    comments = [json.loads(c.decode('utf-8')) for c in comments]
+    return json.dumps(comments)
 
 
 @app.route('/ticket/<int:ticket_id>/comment', methods=['POST'])
 def ticket_comment_create(ticket_id):
     req_json = request.json
-    new_comment = Comment(
-        ticket_id=ticket_id,
-        text=req_json.get('text'),
-        email=req_json.get('email')
-    )
-    db.session.add(new_comment)
-    db.session.commit()
-    return new_comment.to_dict()
+    comment_dict = {
+        "id": str(uuid.uuid4()),
+        "text": req_json.get('text'),
+        "email": req_json.get('email'),
+        "created_at": str(datetime.utcnow()),
+    }
+    r.rpush(REDIS_TICKET_COMMENTS_KEY_TEMPLATE.format(ticket_id=ticket_id), json.dumps(comment_dict))
+    return comment_dict
 
 
-@app.route('/ticket/<int:ticket_id>/comment/<int:comment_id>/delete', methods=['POST'])
+@app.route('/ticket/<int:ticket_id>/comment/<comment_id>/delete', methods=['POST'])
 def ticket_comment_delete(ticket_id, comment_id):
-    db.session.query(Comment).filter_by(id=comment_id, ticket_id=ticket_id).delete()
-    db.session.commit()
-    return "Success"
+    comments = r.lrange(REDIS_TICKET_COMMENTS_KEY_TEMPLATE.format(ticket_id=ticket_id), 0, -1)
+    comments = [json.loads(c.decode('utf-8')) for c in comments]
+    new_comments = list(filter(lambda c: c['id'] != comment_id, comments))
+    new_comments_dumped = [json.dumps(c) for c in new_comments]
+    r.delete(REDIS_TICKET_COMMENTS_KEY_TEMPLATE.format(ticket_id=ticket_id))
+    r.lpush(REDIS_TICKET_COMMENTS_KEY_TEMPLATE.format(ticket_id=ticket_id), *new_comments_dumped)
+    return json.dumps(new_comments)
 
 
 if __name__ == '__main__':
